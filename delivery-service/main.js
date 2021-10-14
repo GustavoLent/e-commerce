b7f3cecaa5b1c5110709ab0a1acd083e12087be6
Gustavo 2 <gustavo.lent@unesp.br>
2021-10-14 14:37:45 -0300

    connecting to kafka through docker internal network + refact in .envs

--- delivery-service/main.js
+++ delivery-service/main.js
@@ -1,6 +1,16 @@
 import streamListenerService from "./services/streamListener/streamListenerService.js"
 
 (async function main() {
-  await streamListenerService.initializeConsumer()
-  await streamListenerService.startListening()
+  console.log('Starting consumer...')
+
+  try {
+
+    await streamListenerService.initializeConsumer()
+    await streamListenerService.startListening()
+    console.log('Consumer is listenning!')
+
+  } catch (error) {
+    console.error(`Error initializing consumer. '${error}'`)
+  }
+
 })()
